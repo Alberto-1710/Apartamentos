24a15b3b98f43eed1c8ef18fe22c17b8d188bd9a
Alberto-1710 <ajromero1710@gmail.com>
2024-02-22 00:28:23 -0600

    prueba con formula

--- app/Http/Controllers/CalculoController.php
+++ app/Http/Controllers/CalculoController.php
@@ -10,8 +10,10 @@ class CalculoController extends Controller
 
 public function calculo($area, $numpiso=null){
     if($numpiso!=null){
-        $preciobase = (1500*0.001*($numpiso-1))+1500;
-        $precioFinal = $area*$preciobase;
+        //$preciobase = (1500*0.001*($numpiso-1))+1500;
+        $preciobase = 1500;
+        $incrementoPorPiso = 0.001;
+        $precioFinal = $preciobase * (1 + ($numpiso - 1) * $incrementoPorPiso) * $area;
         return $precioFinal;
     }else{
         $numpiso = 1;
