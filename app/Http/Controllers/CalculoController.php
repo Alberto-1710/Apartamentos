65ddd3d613241b7e0409b1dd84c7730d8b185967
Alberto-1710 <ajromero1710@gmail.com>
2024-02-25 19:28:28 -0600

    correcion formula del aumento

--- app/Http/Controllers/CalculoController.php
+++ app/Http/Controllers/CalculoController.php
@@ -9,15 +9,20 @@ class CalculoController extends Controller
     //
 
 public function calculo($area, $numpiso=null){
+    $preciobase = 1500;
     if($numpiso!=null){
-        //$preciobase = (1500*0.001*($numpiso-1))+1500;
-        $preciobase = 1500;
-        $incrementoPorPiso = 0.001;
-        $precioFinal = $preciobase * (1 + ($numpiso - 1) * $incrementoPorPiso) * $area;
-        return $precioFinal;
+        if($numpiso<=12){
+            $aumento = 1.001; //0.1% + 1
+            $preciobase = 1500 * pow( $aumento, ($numpiso-1));
+            $precioFinal = $area * $preciobase;
+            return $precioFinal;
+        }else{
+            return 'ERROR: el edificio solo cuenta con 12 niveles';
+        }
+        
     }else{
-        $numpiso = 1;
         $preciobase = 1500;
+        $numpiso = 1;
         $precioFinal = $area*$preciobase;
         return $precioFinal;
     }
